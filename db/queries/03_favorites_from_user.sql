7ba0b7c0b80c98adf2c711394918b5b5ddfa51fd
Diego Castro <diegojcr4@gmail.com>
2021-09-20 15:49:32 -0400

    correct output for favorites_from_user query

--- db/queries/03_favorites_from_user.sql
+++ db/queries/03_favorites_from_user.sql
@@ -1,5 +1,5 @@
-SELECT *
+SELECT users.id as user_id, maps.*
 FROM favorites
-JOIN users
-ON favorites.user_id = users.id
+JOIN users ON favorites.user_id = users.id
+JOIN maps ON favorites.map_id = maps.id
 WHERE users.id = 2;
