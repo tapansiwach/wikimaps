7df28549113f41bdf3120b05f218cd96ac3e296c
Diego Castro <diegojcr4@gmail.com>
2021-09-20 13:47:33 -0400

    change pin URLs

--- db/seeds/05_pins.sql
+++ db/seeds/05_pins.sql
@@ -1,7 +1,7 @@
 INSERT INTO pins (title, description, img_url, lat, long, map_id, user_id) VALUES ('Example pin', 'Look!', 'https://i.imgur.com/c50a1se.jpg', 63.20417, 9.93993, 1, 1);
-INSERT INTO pins (title, description, img_url, lat, long, map_id, user_id) VALUES ('Another pin', 'Description goes here.', 'https://i.imgur.com/GmdNUpB.png', 63.20413, 9.93988, 1, 2);
+INSERT INTO pins (title, description, img_url, lat, long, map_id, user_id) VALUES ('Another pin', 'Description goes here.', 'https://i.imgur.com/ZHly63t.jpg', 63.20413, 9.93988, 1, 2);
 INSERT INTO pins (title, description, img_url, lat, long, map_id, user_id) VALUES ('Third pin', 'Other stuff.', 'https://i.imgur.com/vCxIbG6.jpg', 63.20409, 9.93999, 1, 3);
-INSERT INTO pins (title, description, img_url, lat, long, map_id, user_id) VALUES ('Some other pin', 'Description goes here.', 'https://i.imgur.com/H4nLdH0.png', 21.02160, 71.74744, 2, 6);
-INSERT INTO pins (title, description, img_url, lat, long, map_id, user_id) VALUES ('Some other pin', 'Cool thing here.', 'https://i.imgur.com/H4nLdH0.png', 21.02273, 71.74961, 2, 7);
-INSERT INTO pins (title, description, img_url, lat, long, map_id, user_id) VALUES ('Some other pin', 'Hey look at this.', 'https://i.imgur.com/H4nLdH0.png', 21.02277, 71.74796, 2, 8);
-INSERT INTO pins (title, description, img_url, lat, long, map_id, user_id) VALUES ('Some other pin', 'Stuff.', 'https://i.imgur.com/H4nLdH0.png', 21.02080, 71.74885, 2, 9);
+INSERT INTO pins (title, description, img_url, lat, long, map_id, user_id) VALUES ('Some other pin', 'Description goes here.', 'https://i.imgur.com/lqXJsCs.jpg', 21.02160, 71.74744, 2, 6);
+INSERT INTO pins (title, description, img_url, lat, long, map_id, user_id) VALUES ('Some other pin', 'Cool thing here.', 'https://i.imgur.com/m0rKEXH.jpg', 21.02273, 71.74961, 2, 7);
+INSERT INTO pins (title, description, img_url, lat, long, map_id, user_id) VALUES ('Some other pin', 'Hey look at this.', 'https://i.imgur.com/H5oN1eT.jpg', 21.02277, 71.74796, 2, 8);
+INSERT INTO pins (title, description, img_url, lat, long, map_id, user_id) VALUES ('Some other pin', 'Stuff.', 'https://i.imgur.com/Jc06c53.jpg', 21.02080, 71.74885, 2, 9);
