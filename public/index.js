65d4faf3a58c6c3ed4919817c040f5963a637a19
Tapan Siwach <siwach.tapan@gmail.com>
2021-09-22 12:01:01 -0400

    obtain lat and lon when map is clicked

--- public/index.js
+++ public/index.js
@@ -3,19 +3,59 @@ let latt;
 let lngg;
 let zoomm;
 
-function setCoords(latt,lngg, zoomm){
+function setCoords(latt, lngg, zoomm) {
   this.latt = latt;
   this.lngg = lngg;
   this.zoomm = zoomm
 }
 
-var GoogleMaps = GoogleMaps || {};
+// var GoogleMaps = GoogleMaps || {};
 
-GoogleMaps.initMap = function () {
-  return function () {
-    map = new google.maps.Map(document.getElementById("map"), {
-      center: { lat: this.latt, lng: this.lngg },
-      zoom: this.zoomm,
+// GoogleMaps.initMap = function() {
+//   return function() {
+//     map = new google.maps.Map(document.getElementById("map"), {
+//       center: { lat: this.latt, lng: this.lngg },
+//       zoom: this.zoomm,
+//     });
+
+//     // Create the initial InfoWindow.
+//     let infoWindow = new google.maps.InfoWindow({
+//       content: "Click the map to get Lat/Lng!",
+//       position: myLatlng,
+//     });
+
+//     infoWindow.open(map);
+
+//   }()
+// }
+
+
+function initMap() {
+  const myLatlng = { lat: this.latt, lng: this.lngg };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: this.zoomm,
+    center: myLatlng,
+  });
+  // Create the initial InfoWindow.
+  let infoWindow = new google.maps.InfoWindow({
+    content: "Click the map to get Lat/Lng!",
+    position: myLatlng,
+  });
+
+  infoWindow.open(map);
+  // Configure the click listener.
+  map.addListener("click", (mapsMouseEvent) => {
+    console.log(mapsMouseEvent.latLng.toJSON());
+    // Close the current InfoWindow.
+    infoWindow.close();
+    // Create a new InfoWindow.
+    infoWindow = new google.maps.InfoWindow({
+      position: mapsMouseEvent.latLng,
     });
-  }()
+    infoWindow.setContent(
+      JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+    );
+    infoWindow.open(map);
+  });
 }
+
