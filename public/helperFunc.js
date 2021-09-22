7f14c7b28ecd8b20438347ae17c888400ad4c225
Diego Castro <diegojcr4@gmail.com>
2021-09-22 15:46:37 -0400

    work in progress for share button modal

---
+++ public/helperFunc.js
@@ -0,0 +1,24 @@
+
+let mapId = 1;
+let modal = document.getElementById('exampleModal');
+let modalInputform;
+
+function shareFunc(map_id) {
+  console.log(`Clicked the Share button for id: ${map_id}`);
+  mapId = map_id;
+
+  var modalTitle = modal.querySelector('.modal-title');
+  modalInputform = modal.querySelector('.form-control');
+  // Clear the form's email value every time the modal pops up.
+  modalInputform.value = '';
+
+  modalTitle.textContent = `Share map #${map_id}!`;
+  // var modalBodyInput = mod.querySelector('.modal-body input')
+}
+
+function addCollaborator() {
+  console.log(`addCollaborator() called from save button for map#${mapId}`)
+
+  console.log(`Here we can do a INSERT INTO authorizations WHERE email = ${modalInputform.value}`);
+
+}
