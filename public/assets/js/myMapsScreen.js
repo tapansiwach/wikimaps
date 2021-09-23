20595a567a8b72a3df95c25b2e4ab5d8e62606df
Tapan Siwach <siwach.tapan@gmail.com>
2021-09-23 10:36:18 -0400

    change onClickListener from row to map title on myMaps second & third tables

--- public/assets/js/myMapsScreen.js
+++ public/assets/js/myMapsScreen.js
@@ -4,12 +4,14 @@ const port = "8080"
 document.addEventListener('DOMContentLoaded', () => {
 
   document.querySelectorAll("tr .dataholder_title").forEach(td => {
+
     td.addEventListener('click', function() {
       console.log(`clicked on map id ${this.getAttribute('data-mapId')}`);
       const mapId = this.getAttribute('data-mapId');
 
       window.location.replace(`/test-map${mapId}`);
     });
+
   });
 
 });
